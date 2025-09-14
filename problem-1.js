d02e342140e76d3f5775b0530136ca9f2a4f6555
muhib003 <muhib.kagozi03@gmail.com>
2025-09-15 03:25:54 +0600

    problem-1.js is done

--- problem-1.js
+++ problem-1.js
@@ -1,3 +1,7 @@
 function totalFine( fare ) {
-    
+    if(typeof fare !== "number" || fare <= 0){
+        return("Invalid");
+    };
+    const fine = fare + ((fare * 20) / 100) + 30;
+    return fine;
 }
